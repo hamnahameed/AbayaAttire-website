67ab3fdb90136e37eba21e588e55e141e04bec54
hamnahameed <64436771+hamnahameed@users.noreply.github.com>
2023-03-02 00:59:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting){
+//             document.querySelectorAll(".card")[0].classList.add("fadeInLeft");
+//             document.querySelectorAll(".card")[1].classList.add("fadeInTop");
+//             document.querySelectorAll(".card")[2].classList.add("fadeInRight");
+//         }
+//     })
+//  })
+
+//     observer.observe(document.querySelector(". card-group" ));
+
+AOS.init({
+    easing: 'ease-out-back',
+    duration: 1000
+    });
+
+  
+
+
+    // magnifier
+  
+    
